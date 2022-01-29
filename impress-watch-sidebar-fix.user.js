6eadb1194e88d7807fc1858ba5c4d7b7dbb52236
Sigsign <sig@signote.cc>
2022-01-29 21:22:24 +0900

    Version 0.2.0

--- impress-watch-sidebar-fix.user.js
+++ impress-watch-sidebar-fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name      Impress Watch: Sidebar fix
 // @namespace https://github.com/sigsignv/userjs-impress-watch-sidebar-fix
-// @version   0.1.0
+// @version   0.2.0
 // @author    Sigsign
 // @license   MIT or Apache-2.0
 // @match     https://*.watch.impress.co.jp/docs/*
@@ -12,7 +12,10 @@
 (function () {
 'use strict';
 
-const delayFirstContentfulPaint = () => {
+/**
+* First Contentful Paint (FCP) を DOMContentLoaded 以降に遅延させてチラつきを防止する
+*/
+function delayFirstContentfulPaint() {
     if (document.readyState !== 'loading') {
         return;
     }
@@ -22,31 +25,47 @@ const delayFirstContentfulPaint = () => {
             document.documentElement.style.visibility = '';
         });
     }, { once: true });
-};
-const delaySidebarRendering = () => {
+}
+/**
+* サイドバーを一時的に隠すことでレイアウトシフトの回数を減らす
+*/
+function delaySidebarRendering() {
     document.addEventListener('DOMContentLoaded', () => {
+        // サイドバー
         const sidebar = document.querySelector('aside#extra aside.ad');
         if (!sidebar) {
             return console.error('Sidebar is not exists');
         }
+        // サイドバーの広告以外で一番上の要素
         const firstElement = sidebar.querySelector('aside:first-of-type');
         if (!firstElement) {
             return console.error('Sidebar is broken');
         }
+        // 値を保存
         const values = {
             marginTop: firstElement.style.marginTop,
             visibility: sidebar.style.visibility,
         };
-        sidebar.style.visibility = 'hidden';
-        firstElement.style.marginTop = '3000px';
+        // サイドバーを隠す
+        requestAnimationFrame(() => {
+            sidebar.style.visibility = 'hidden';
+            firstElement.style.marginTop = '3000px';
+        });
+        // 数秒後にサイドバーを戻す
         setTimeout(() => {
             requestAnimationFrame(() => {
+                const posY = window.scrollY;
                 firstElement.style.marginTop = values.marginTop;
                 sidebar.style.visibility = values.visibility;
+                // スクロール位置が一定以上動いていたら戻す
+                if (Math.abs(posY - window.scrollY) > 300) {
+                    console.log('Restore scroll position');
+                    window.scroll(0, posY);
+                }
             });
         }, 5 * 1000);
     }, { once: true });
-};
+}
 delaySidebarRendering();
 delayFirstContentfulPaint();
 
--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ interface PreviousValues {
     visibility: string
 }
 
-const delayFirstContentfulPaint = () => {
+/**
+* First Contentful Paint (FCP) を DOMContentLoaded 以降に遅延させてチラつきを防止する
+*/
+function delayFirstContentfulPaint() {
     if (document.readyState !== 'loading') {
         return
     }
@@ -15,26 +18,42 @@ const delayFirstContentfulPaint = () => {
     }, { once: true })
 }
 
-const delaySidebarRendering = () => {
+/**
+* サイドバーを一時的に隠すことでレイアウトシフトの回数を減らす
+*/
+function delaySidebarRendering() {
     document.addEventListener('DOMContentLoaded', () => {
+        // サイドバー
         const sidebar = document.querySelector<HTMLElement>('aside#extra aside.ad')
         if (!sidebar) {
             return console.error('Sidebar is not exists')
         }
+        // サイドバーの広告以外で一番上の要素
         const firstElement = sidebar.querySelector<HTMLElement>('aside:first-of-type')
         if (!firstElement) {
             return console.error('Sidebar is broken')
         }
+        // 値を保存
         const values: PreviousValues = {
             marginTop: firstElement.style.marginTop,
             visibility: sidebar.style.visibility,
         }
-        sidebar.style.visibility = 'hidden'
-        firstElement.style.marginTop = '3000px'
+        // サイドバーを隠す
+        requestAnimationFrame(() => {
+            sidebar.style.visibility = 'hidden'
+            firstElement.style.marginTop = '3000px'
+        })
+        // 数秒後にサイドバーを戻す
         setTimeout(() => {
             requestAnimationFrame(() => {
+                const posY = window.scrollY
                 firstElement.style.marginTop = values.marginTop
                 sidebar.style.visibility = values.visibility
+                // スクロール位置が一定以上動いていたら戻す
+                if (Math.abs(posY - window.scrollY) > 300) {
+                    console.log('Restore scroll position')
+                    window.scroll(0, posY)
+                }
             })
         }, 5 * 1000);
     }, { once: true })
